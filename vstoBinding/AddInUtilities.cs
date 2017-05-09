303ff857900544b53275d7a3a4557f1843f2717e
vermosen <vermosen.jm@free.fr>
2017-05-09 07:55:41 -0400

    added function

--- vstoBinding/AddInUtilities.cs
+++ vstoBinding/AddInUtilities.cs
@@ -13,6 +13,7 @@ namespace vstoBinding
     public interface IAddInUtilities
     {
         void ImportData();
+        void exchangeData([In, MarshalAs(UnmanagedType.BStr)] string strFilename);
     }
 
     [ComVisible(true)]
@@ -26,8 +27,19 @@ namespace vstoBinding
 
             if (activeWorksheet != null)
             {
-                Excel.Range range1 = activeWorksheet.get_Range("A1", System.Type.Missing);
-                range1.Value2 = "This is my data";
+                Excel.Range rng = activeWorksheet.get_Range("A1", Type.Missing);
+                rng.Value2 = "Hello world !";
+            }
+        }
+
+        public void exchangeData(string str)
+        {
+            Excel.Worksheet activeWorksheet = Globals.ThisAddIn.Application.ActiveSheet as Excel.Worksheet;
+
+            if (activeWorksheet != null)
+            {
+                Excel.Range rng = activeWorksheet.get_Range("A2", Type.Missing);
+                rng.Value2 = str;
             }
         }
     }
